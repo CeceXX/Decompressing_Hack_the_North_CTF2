f28ce5dc2be4d81e8b50bf5c4235d9f672aaa20e
CeceXX <cesaredecal@NewMacbook.lan>
2015-09-13 14:13:43 +0200

    Added the files we should work with

---
+++ decompressing.py
@@ -0,0 +1,9 @@
+import argparse
+
+if __name__ == "__main__": 
+    print("I'm in main!")
+    parser = argparse.ArgumentParser(description='Decompressing has never been easier!')
+    parser.add_argument('--decompress', dest='compress', action='store_const',
+                       const=False, default=True)
+    args = parser.parse_args()
+
