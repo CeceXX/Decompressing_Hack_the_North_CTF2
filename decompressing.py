76e39ff72f53111d33dcfaafead18a3a3de5ed52
CeceXX <cesaredecal@NewMacbook.lan>
2015-09-13 14:35:01 +0200

    Finished coding the s_decompress method

--- decompressing.py
+++ decompressing.py
@@ -1,9 +1,30 @@
 # Minimal code to decompress.
+# 1st -> main
+# 2nd ->s_decompress
 
 import argparse
 
+def matdiv(c, b):
+    return numpy.divide(c, b)
+
 def s_decompress(x):
     print("I'm in decompress!")
+    
+    def s_decompress(x):
+    print("I'm in decompress!")
+    version, x = int(x[:3]), x[3:]
+    head, body, foot, ignore = x.split('\n')
+    key, mkey, *head = head.split('/')
+    mkey = eval(mkey)
+    chunks = r_sort(head[:-1])
+    print(len(chunks))
+    def matrix_fix(lst):
+        lst = eval(lst.group(1))
+        soln = matdiv(lst, mkey)
+        if soln[0][1] == 0:
+            return chr(int(soln[0][0]))
+        else:
+            return '@' + ''.join(str(int(i)-10) for i in soln[0])
 
 if __name__ == "__main__": 
     print("I'm in main!")
